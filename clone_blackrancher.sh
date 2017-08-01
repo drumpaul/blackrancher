508ee7cd37ec97c17f78fbc8c9ca7402894fccb7
plucia-link <plucia@linkgroup.it>
2017-08-01 20:25:05 +0200

    clone script definitivo

--- clone_blackrancher.sh
+++ clone_blackrancher.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/drumpaul/blackrancher.git
+git="docker run -ti --rm -v $(pwd):/git bwits/docker-git-alpine"
+clone="clone"
+url="https://github.com/drumpaul/blackrancher.git"
+
+$git $clone $url
--- enable_git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-start_git()
-{
-alias git="docker run -ti --rm -v $(pwd):/git bwits/docker-git-alpine"
-}
-
-export -f start_git
