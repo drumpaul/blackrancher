c80e2888d17795110564c00a302bd42ff0675a87
drumpaul <drumpaul@users.noreply.github.com>
2017-09-08 17:46:01 +0200

    Update run_deluge.sh

--- deluge/run_deluge.sh
+++ deluge/run_deluge.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f delugeweb
-docker run --name=delugeweb -d -v "/home/cotlod:/data" -p 53160 -p 8112:8112 -p 53160/udp -p 58846 cotlod/delugeweb
+docker run --name=delugeweb -d -v "/home/cotlod:/data" --expose=53160 -p 8112:8112 --expose=53160/udp --expose=58846 cotlod/delugeweb
