14c9937fbac1f5c131fc34e2820432f9c750f3c4
drumpaul <drumpaul@users.noreply.github.com>
2019-07-26 19:14:56 +0200

    Update run_deluge.sh

--- deluge/run_deluge.sh
+++ deluge/run_deluge.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-docker rm -f delugeweb
-docker run --name=delugeweb -d -v "/home/cotlod:/data" --expose=53160 -p 8112:8112 --expose=53160/udp --expose=58846 cotlod/delugeweb
+#docker rm -f delugeweb
+#docker run --name=delugeweb -d -v "/home/cotlod:/data" --expose=53160 -p 8112:8112 --expose=53160/udp --expose=58846 cotlod/delugeweb
+docker run \
+  --name=deluge \
+  --net=host \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e UMASK_SET=022 \
+  -e TZ=timezone \
+  -v /home/rancher/conf:/config \
+  -v /home/rancher/dati:/downloads \
+  --restart unless-stopped \
+  linuxserver/deluge
